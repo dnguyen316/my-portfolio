0c850d9f74e2f5cb54fcf0e666eee95d7d9195a0
dnguyen316 <ngltduong@gmail.com>
2024-03-16 15:33:23 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/my-portfolio/",
   test: {
     environment: "jsdom",
     setupFiles: ["./src/tests/setup.ts"],
