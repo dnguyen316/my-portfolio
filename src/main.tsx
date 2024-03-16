9e0f778264d5cffc5594059b701cb648f048d339
dnguyen316 <ngltduong@gmail.com>
2024-03-16 17:06:55 +0700

    remove sidebar lib

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App.tsx";
 import "./index.css";
 import ThemeProvider from "./Containers/Theme/ThemeProvider.tsx";
-import { ProSidebarProvider } from "react-pro-sidebar";
+// import { ProSidebarProvider } from "react-pro-sidebar";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <ProSidebarProvider>
-        <App />
-      </ProSidebarProvider>
+      <App />
+      {/* <ProSidebarProvider>
+      </ProSidebarProvider> */}
     </ThemeProvider>
   </React.StrictMode>
 );
